96a9116582e72747e7b21078c4e662574215f3a8
Simeone Vilardo <simeone.vilardo@hotmail.com>
2017-09-09 21:08:00 +0200

    Add consumer and business login functions Refactored config files and main files Add base code to create folders structure

---
+++ OneDriveUploader/config.py
@@ -0,0 +1,10 @@
+client = dict(
+    id = '8dfcc6ca-304f-4351-a2c9-299e72eb8605'
+)
+
+urls = dict(
+    redirect = 'http://localhost:8080',
+    discovery = 'https://api.office.com/discovery/',
+    auth_server = 'https://login.microsoftonline.com/common/oauth2/authorize',
+    auth_token = 'https://login.microsoftonline.com/common/oauth2/token'
+)
