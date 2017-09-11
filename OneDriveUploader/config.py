8b9124dc957b1ee1626dd227f6f709b8700dfdb8
Cosimo Russo <cosimo@menuale.com>
2017-09-11 09:36:30 +0200

    Change local web server's URL

--- OneDriveUploader/config.py
+++ OneDriveUploader/config.py
@@ -6,7 +6,7 @@ load_dotenv(find_dotenv())
 
 client = dict(id = '8dfcc6ca-304f-4351-a2c9-299e72eb8605')
 
-urls = dict(redirect = 'http://localhost:8080',
+urls = dict(redirect = 'http://localhost:8081',
     discovery = 'https://api.office.com/discovery/',
     auth_server = 'https://login.microsoftonline.com/common/oauth2/authorize',
     auth_token = 'https://login.microsoftonline.com/common/oauth2/token')
