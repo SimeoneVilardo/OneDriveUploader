10cf65fcfc2466fab412c74cf55256b1da64d14f
Simeone Vilardo <simeone.vilardo@hotmail.com>
2017-09-14 02:05:10 +0200

    New create_folder_structure logic to reduce the search of a folder to the minmum

--- OneDriveUploader/one_drive_engine.py
+++ OneDriveUploader/one_drive_engine.py
@@ -8,19 +8,51 @@ class OneDriveEngine():
 
     client = None
 
+
     '''
     Ottiene una lista di stringhe che contiene tutte le cartelle che formano il percorso del parametro path.
     '''
-    def get_dir_components(self, path):
-        if path != None:
-            dir_components_raw =  os.path.normpath(path).split(os.path.sep)
-            dir_components = [x for x in dir_components_raw if x != '']
-            return dir_components
-        else:
-            return ''
+    def get_path_components(self, path):
+        return [x for x in os.path.normpath(path).split(os.path.sep) if x != ''] if path else []
+
 
     def get_basename(self, path):
         return os.path.basename(path).replace(os.path.sep, '')
+
+
+    def get_common_path(self, path1, path2):
+        common_path = ''
+        path1_len = len(path1)
+        path2_len = len(path2)
+        for i in range(path1_len if path1_len >= path1_len else path2_len):
+            if path1[i] == path2[i]:
+                common_path = common_path + path1[i]
+            else:
+                break
+        return common_path
+              
+
+    def get_diff_paths(self, path1, path2, root_path=''):
+        if path1[-1] == os.path.sep:
+            path1 = path1[:-1]
+        if path2[-1] == os.path.sep:
+            path2 = path2[:-1]
+        path1 = path1.replace(root_path, '')
+        path2 = path2.replace(root_path, '')
+        common_path = self.get_common_path(path1, path2)
+        path1 = path1.replace(common_path, '')
+        path2 = path2.replace(common_path, '')
+        path1_depth = len(self.get_path_components(path1))
+        path2_depth = len(self.get_path_components(path2))
+        if path1_depth == path2_depth:
+            return (0, '', 0)
+        elif path1_depth > path2_depth:
+            path_diff = path1.replace(path2, '')
+            return (1, path_diff, len(self.get_path_components(path_diff)))
+        elif path2_depth > path1_depth:
+            path_diff = path2.replace(path1, '')
+            return (2, path_diff, len(self.get_path_components(path_diff)))
+
 
     '''
     Ottiene una lista di stringhe che contiene tutte le cartelle contenute nella cartella specificata nel parametro path.
@@ -30,17 +62,20 @@ class OneDriveEngine():
     def get_all_dirs(self, path):
         return [root for root, subdirs, files in os.walk(path)]
 
+
     '''
     Ottiene un oggetto di tipo cartella da OneDrive il cui id corrisponde all'id specificato nel parametro id.
     '''
-    def get_folder_by_id(self, id):
-        return self.client.item(drive='me', id=id)
+    def get_object_by_id(self, id, drive='me'):
+        return self.client.item(drive=drive, id=id)
+
 
     '''
     Ottiene l'oggetto di tipo cartella da OneDrive il cui id è root.
     '''
     def get_root_folder(self):
-        return self.get_folder_by_id('root')
+        return self.get_object_by_id('root')
+
 
     '''
     Ottiene un oggetto di tipo cartella da OneDrive dato il nome della cartella da trovare ed eventualmente
@@ -48,13 +83,14 @@ class OneDriveEngine():
     ATTENZIONE: Questa funzione analizza solo un livello, che può essere il root o quello diversamente specificato
     dal parametro start_folder. Il parametro start_folder deve essere un oggetto di tipo cartella.
     '''
-    def get_folder_by_dirname(self, dir_name, start_folder=None):
+    def get_folder_by_name(self, name, start_folder=None):
         current_folder = start_folder if start_folder != None else self.get_root_folder()
         if current_folder.children != None:
             for item in current_folder.children.get():
-                if item.folder != None and dir_name == item.name:
-                    return self.get_folder_by_id(item.id)
+                if item.folder != None and name == item.name:
+                    return self.get_object_by_id(item.id)
         return None
+
 
     '''
     Ottiene un oggetto di tipo cartella da OneDrive dato il path della cartella da trovare ed eventualmente
@@ -67,15 +103,15 @@ class OneDriveEngine():
     booleano del primo elemento della tupla.
     3) Lista di stringhe, è la sequenza di cartelle che NON sono state trovate, quindi non esistono su OneDrive.
     '''
-    def get_folder_by_path(self, folder_path, start_folder=None):
+    def get_folder_by_path(self, path, start_folder=None):
         current_folder = start_folder if start_folder != None else self.get_root_folder()
-        dir_components = self.get_dir_components(folder_path)
-        while len(dir_components) > 0:
-            temp_folder = self.get_folder_by_dirname(dir_components[0], current_folder)
+        path_components = self.get_path_components(path)
+        while len(path_components) > 0:
+            temp_folder = self.get_folder_by_name(path_components[0], current_folder)
             if temp_folder == None:
-                return (False, current_folder, dir_components)
+                return (False, current_folder, path_components)
             current_folder = temp_folder
-            dir_components.pop(0)
+            path_components.pop(0)
         return (True, current_folder, None)
 
 
@@ -84,37 +120,67 @@ class OneDriveEngine():
     e lo inserisce tra gli oggetti figli dell'oggetto specificato nel parametro parent.
     Il dato ritornato è l'oggetto di tipo cartella appena inserito.
     '''
-    def create_folder_by_name(self, parent, name):
+    def create_folder_by_name(self, name, parent=None, ignore_exists=True):
         parent = parent if parent != None else self.get_root_folder()
+        if (not ignore_exists) and self.get_folder_by_name(name, parent) != None:
+            raise Exception('Folder already exists and overwrite flag is set to false.')
         f = onedrivesdk.Folder()
         i = onedrivesdk.Item()
         i.name = name
         i.folder = f
         new_folder = parent.children.add(i)
-        return self.get_folder_by_id(new_folder.id)
+        return self.get_object_by_id(new_folder.id)
 
-    def create_folder_by_path(self, folder_path, start_folder_path=None):
-        current_folder = self.get_folder_by_path(start_folder_path)[1] if start_folder_path != None else self.get_root_folder()
-        folder_components = self.get_dir_components(folder_path)
-        for folder_name in folder_components:
-            current_folder_temp = self.get_folder_by_dirname(folder_name, current_folder)
-            current_folder = current_folder_temp if current_folder_temp != None else self.create_folder_by_name(current_folder, folder_name)
-        return current_folder    
+
+    def create_folder_by_path(self, path, start_folder=None, ignore_exists=True):
+        current_folder = start_folder if start_folder != None else self.get_root_folder()
+        path_components = self.get_path_components(path)
+        created = False
+        for folder_name in path_components:
+            current_folder_temp = self.get_folder_by_name(folder_name, current_folder) # creato allo scopo di non far eseguire il codice due volte nella prossima riga
+            if folder_name == path_components[-1]:
+                if current_folder_temp != None:
+                    current_folder = current_folder_temp
+                    created = False
+                    if not ignore_exists:
+                        raise Exception('Folder already exists and overwrite flag is set to false.')
+                else:
+                    current_folder = self.create_folder_by_name(folder_name, current_folder)
+                    created = True
+        return [created, current_folder]
+
 
     '''
     Crea l'intera struttura ad albero delle cartelle partendo dalla cartella specificata nel parametro dir_path all'interno
     della root o eventualmente all'interno dell'oggetto di tipo cartella specificato nel parametro start_folder.
     '''
-    def create_folder_structure(self, dir_path, start_folder_path=None):
-        start_folder = self.get_folder_by_path(start_folder_path) if start_folder_path != None else self.get_root_folder()
-        first_folder_name = self.get_basename(dir_path)
-        self.create_folder_by_name(start_folder, first_folder_name)
-        start_folder = self.get_folder_by_dirname(first_folder_name, start_folder)
-        for root, subdirs, files in os.walk(dir_path):
-            current_folder = self.get_folder_by_path(root.replace(dir_path, '') if root != dir_path else first_folder_name, start_folder)[1]
-            for subdir in subdirs:
-                self.create_folder_by_name(current_folder, subdir)
-                
+    def create_folder_structure(self, dir_path, start_folder_path=None, ignore_exists=True):
+        current_folder = self.get_folder_by_path(start_folder_path) if start_folder_path != None else self.get_root_folder()
+        folders_cache = list()
+        all_dirs = self.get_all_dirs(dir_path)
+        current_folder_path = all_dirs[0]
+        folder_name = self.get_basename(current_folder_path)
+        current_folder = self.create_folder_by_name(folder_name, current_folder)
+        all_dirs = all_dirs[1:]
+        for i in range(len(all_dirs)):
+            folder_name = self.get_basename(all_dirs[i])
+            self.create_folder_by_name(folder_name, current_folder)
+            if i < len(all_dirs)-1:
+                path_diff = self.get_diff_paths(current_folder_path, all_dirs[i+1], dir_path)
+                if path_diff[0] == 2 and path_diff[2] > 1:
+                    current_folder_path = all_dirs[i]
+                    folders_cache.append(current_folder)
+                    current_folder = self.get_folder_by_name(folder_name, current_folder)
+                elif path_diff[0] == 0:
+                    current_folder_path = current_folder_path.replace(os.path.sep + self.get_path_components(current_folder_path)[-1], '')
+                    current_folder = folders_cache[-1]
+                    folders_cache = folders_cache[:-1]
+                elif path_diff[0] == 1 and path_diff[2] > 1:
+                    current_folder_path = current_folder_path.replace(path_diff[1], '')
+                    current_folder = folders_cache[-path_diff[2]]
+                    folders_cache = folders_cache[:-path_diff[2]]
+               
+                 
     def login_business(self, redirect_uri, client_id, client_secret, discovery_uri, auth_server_url, auth_token_url):
         http = onedrivesdk.HttpProvider()
         auth = onedrivesdk.AuthProvider(http,
@@ -133,6 +199,7 @@ class OneDriveEngine():
             client = onedrivesdk.OneDriveClient(service_info.service_resource_id + '/_api/v2.0/', auth, http)
             self.client = client
             return True
+
 
     def login(self, redirect_uri, client_id, client_secret):
         scopes = ['wl.signin', 'wl.offline_access', 'onedrive.readwrite']
